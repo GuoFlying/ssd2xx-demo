32aba7395c36cb8a548132a3674048bfd8ebd32d
Flying <weekend@111.com>
2021-09-22 23:54:21 -0700

    1、修改gif demo：从文件系统中读取gif文件并加载显示

--- 4.littlevgl/f_apps/lv_lib_gif_test_app/main.c
+++ 4.littlevgl/f_apps/lv_lib_gif_test_app/main.c
@@ -2,10 +2,30 @@
 #include "lvgl/lvgl.h"
 #include "lv_lib_gif/lv_gif.h"
 #include <assert.h>
+#include "lv_fs_if/lv_fs_if.h"
 
 void f_app_main(int argc, char *argv[])
 {
+    char path[256] = {0};
 
+    if (argc >= 4)
+    {
+        sprintf(path,"S%s",argv[3]);
+    }
+    else
+    {
+        fprintf(stderr, "error: Usage: %s [w,h] [path]\r\n"
+                        "eg: %s 1024 600 ./res/example.gif ",
+                argv[0], argv[0]);
+        assert(argv[3]);
+        return;
+    }
+
+    lv_fs_if_init();
+    lv_obj_t * img = lv_gif_create_from_file(lv_scr_act(), path);
+    lv_obj_center(img);
+
+#if 0
     extern const uint8_t example_gif_map[];  /*"lv_lib_gif/example_gif.h"*/
     lv_obj_t * img = lv_gif_create_from_data(lv_scr_act(), example_gif_map);
     lv_obj_center(img);
@@ -21,4 +41,5 @@ void f_app_main(int argc, char *argv[])
     lv_anim_set_ready_cb(&a, NULL);
 
     lv_anim_start(&a);
+#endif
 }
